2b017e8f7311e42a771bb4dd96508913c8ac131a
Francois Mavunila <mavunila002@gmail.com>
2025-01-05 18:59:56 +0200

    added init message from database

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ from pydantic import BaseSettings
 from fastapi_jwt_auth import AuthJWT
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
-from app.routers import auth
+from app.routers import auth, chatbot
+from typing import List, Dict
+from datetime import timedelta
+import openai
 import os
 
 # App Initialization
@@ -22,7 +25,8 @@ app.add_middleware(
 # Settings class
 class Settings(BaseSettings):
     authjwt_secret_key: str = os.getenv("JWT_SECRET_KEY", "your_secret_key")
-    
+    authjwt_access_token_expires: timedelta = timedelta(days=7)  # Token expires in 30 minutes
+
     class Config:
         env_file = ".env"
 
@@ -33,6 +37,7 @@ def get_config():
 
 # Include Routers
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(chatbot.router, prefix="/chat", tags=["Chat"])
 
 # Root Route
 @app.get("/")
